32692fa246961657ed83a93e039544f388ca829c
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-07-23 11:06:44 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,12 @@
+famous_person = "Kevin Hart"
+is_active = true
+famous_person = "Ashton Kutcher"
+
+num_3 = 50
+num_9 = 27
+sum = num_3 + num_9
+
+animal = ["koala", "kangeroo", "wallaby"]
+animal.delete_at(0)
+
+p famous_person.length
