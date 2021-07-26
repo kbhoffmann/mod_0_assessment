008fd5ad93fda10b18e15e5c79e3cace7e84a7a2
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-07-26 11:33:06 -0600

    Delete models add models2

---
+++ models2/model_a.rb
@@ -0,0 +1,11 @@
+school =
+  {
+    "school name" => "Thomas Jefferson Middle School",
+    "location" => "Port Washington, Wisconsin",
+    "number of kids" => 427,
+    "number of teachers" => 38,
+    "has a pool" => true,
+    "on summer vacation" => false,
+    "grades in school" => [5, 6, 7, 8],
+    "school colors" => ["blue", "yellow"]
+  }
---
+++ models2/model_b.rb
@@ -0,0 +1,11 @@
+pet =
+  {
+  "type of animal" => "Parakeet bird",
+  "name" => "Chi Chi",
+  "age" => 6,
+  "weight in ounces" => 12.5,
+  "talks" =>true,
+  "flys" => true,
+  "colors" => ["light blue", "white", "black"],
+  "eats" => ["eggs", "bird seed", "fruit", "chicken"]
+  }
